71066a6461c43297ebca133c35ce69c2927d3007
njayawa <nexpedition@yahoo.com>
2014-01-06 02:09:02 -0800

    Initial checkin

---
+++ TestMSAS/Session.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using ehMSASLib;
+using Microsoft.Win32;
+
+namespace TestMSAS
+{
+    class Session : MediaStatusSession
+    {
+        void IMediaStatusSession.MediaStatusChange(Array tags,
+                                                   Array properties)
+        {
+            Sink.TraceInformation("Session.MediaStatusChange called");
+
+            for (int i = 0; i < tags.Length; i++)
+            {
+                MEDIASTATUSPROPERTYTAG tag =
+                  (MEDIASTATUSPROPERTYTAG)tags.GetValue(i);
+                object value = properties.GetValue(i);
+                Sink.TraceInformation("Tag {0}={1}",
+                                       tag,
+                                       value);
+                string tagStr = tag.ToString();
+                if (tagStr.Equals("MSPROPTAG_MediaName"))
+                {
+                    Sink.TraceInformation("Witing to key");
+                    WriteToRegistry("Name", value.ToString());
+                   
+                }
+                else if(tagStr.Equals("MSPROPTAG_TrackNumber"))
+                {
+                    WriteToRegistry("Channel", value.ToString());
+                }
+            }
+        }
+
+        public void WriteToRegistry(String keyStr, String value)
+        {
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey("Software", true))
+            {
+
+                key.CreateSubKey("TestMSAS");
+                using (RegistryKey key2 = key.OpenSubKey("TestMSAS", true))
+                {
+
+
+                    key2.CreateSubKey("ProgramInfo");
+                    using(RegistryKey key3 = key2.OpenSubKey("ProgramInfo", true))
+                    {
+
+                        key3.SetValue(keyStr, value);
+                        Sink.TraceInformation("Wrote to Key{0} Value{1}", keyStr, value);
+                    }
+                }
+            }
+        }
+
+        public void Close()
+        {
+            Sink.TraceInformation(("Session.Close called"));
+        }
+    }
+}
---
+++ TestMSAS/Sink.cs
@@ -0,0 +1,50 @@
+﻿using ehMSASLib;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace TestMSAS
+{
+    [ComVisible(true)]
+    [GuidAttribute("360d8298-97d9-4058-9052-b91efe76d3ae")]
+    public class Sink : MediaStatusSink
+    {
+        public static bool SHOULD_TRACE = false;
+        public Sink()
+        {
+            string logFile = string.Format("{0}MsasSink2.log",
+                System.IO.Path.GetTempPath());
+            Trace.Listeners.Add(new TextWriterTraceListener(logFile));
+            Trace.AutoFlush = true;
+            Sink.TraceInformation("Sink started");
+        }
+
+        public static void TraceInformation(string s, params object[] args)
+        {
+            if (ShouldTrace())
+            {
+                Trace.TraceInformation(s, args);
+            }
+        }
+
+        public static void TraceInformation(string s)
+        {
+            TraceInformation(s, null);
+        }
+
+        public static bool ShouldTrace()
+        {
+            return SHOULD_TRACE;
+        }
+
+        public void Initialize()
+        {
+            Sink.TraceInformation(("Sink.Initialize called"));
+        }
+
+        public MediaStatusSession CreateSession()
+        {
+            Sink.TraceInformation(("Sink.CreateSession called"));
+            return new Session();
+        }
+    }
+}
